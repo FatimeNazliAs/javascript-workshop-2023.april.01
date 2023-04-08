c14ee94603d95dd62681027ba2f0a4cc4d154ff1
Binnur KURT <deepcloudlabs@gmail.com>
2023-04-09 00:41:00 +0300

    refactor mastermind.js and handle wins and loses

--- mastermind.js
+++ mastermind.js
@@ -1,7 +1,7 @@
-class Move{
-    constructor(guess,message){
-        this.guess=guess;
-        this.message=message;
+class Move {
+    constructor(guess, message) {
+        this.guess = guess;
+        this.message = message;
     }
 }
 
@@ -16,162 +16,118 @@ class MastermindViewModel {
         //  max tries: 10
         //  lives: 3
         //  game level: 4
-        
-        this.secret=this.createSecret(3);
-        this.guess=ko.observable();
-        this.tries=ko.observable(0);
-        this.moves=ko.observableArray([]);
-        this.level=ko.observable(3);
-        this.lives=ko.observable(3);
-        this.points=ko.observable(0);
-        
-        console.log(this.secret);
 
+        this.secret = this.createSecret(3);
+        this.guess = ko.observable(123);
+        this.tries = ko.observable(0);
+        this.moves = ko.observableArray([]);
+        this.level = ko.observable(3);
+        this.lives = ko.observable(3);
+        this.counter = ko.observable(60);
+        this.progressBarWidth = ko.computed(() => {
+            return Math.floor((5 * this.counter()) / 3) + "%";
+        })
+        this.timer = setInterval(this.countDown, 1_000);
     }
-    play = () =>{
-        this.tries(this.tries()+1);
-        let message="";
-        let secret=String(this.secret);
-        let guess=String(this.guess());
-        let pos=0;
-        let neg=0;        
-        let isMatch=false;
-        
-       
-        for (let s of secret){
-            for (let g of guess){
-                if (s==g){
-                    isMatch=true;                   
-                }
 
-            } 
-            if (isMatch==true){
-                break;}
-               
-        }        
-        if (isMatch==false){
-            message="no match";
-            this.moves.push(new Move(this.guess(),message));
-
+    countDown = () => {
+        this.counter(this.counter() - 1);
+        if (this.counter() <= 0) {
+            this.playerFails();
         }
-        else if (secret===guess){
-            message="You win and level up";
-            this.winGame(message);
-            
-        }
-
-        else {
-
-            if (this.tries()===10){                
-                
-                this.lives(this.lives()-1);
-                this.looseGame();
-               
-            }
-
-            else if (secret==guess){
-                message="You win and level up";
-                this.winGame(message);
-              
-            }
-
-
-            else{
-                                
-                for (let i=0;i<secret.length;i++){
-            
-                    if (secret[i]===guess[i]){
-                        pos+=1;
-    
+    }
+    evaluate = (number1, number2) => {
+        number1 = number1.toString();
+        number2 = number2.toString();
+        let exactMatch = 0;
+        let partialMatch = 0;
+        for (const [i, digit1] of [...number1].entries()) {
+            for (const [j, digit2] of [...number2].entries()) {
+                if (digit1 === digit2) {
+                    if (i === j) {
+                        exactMatch++;
+                    } else {
+                        partialMatch++;
                     }
-                    else{
-                        for (let s of secret){
-                        
-                            if (s===guess[i]){
-                                neg+=-1;
-                            }
-                        }
-                    }
-    
                 }
-                message=String(neg) +"/"+String(pos);
-                this.moves.push(new Move(this.guess(),message));
-        
-            
+            }
         }
-        }
-   
+        let message = exactMatch === 0 && partialMatch === 0 ? "No Match" : "";
+        if (partialMatch > 0)
+            message = `${message}-${partialMatch}`;
+        if (exactMatch > 0)
+            message = `${message}+${exactMatch}`;
+        return message;
+    }
 
-}
-    initializeGame=()=>{
+    play = () => {
+        this.tries(this.tries() + 1);
+        let secret = this.secret.toString();
+        let guess = this.guess().toString();
+        if (secret.toString() === guess.toString()) {
+            this.playerLevelsUp();
+        } else {
+            if (this.tries() === 10) {
+                this.playerFails();
+            } else {
+                this.moves.push(new Move(this.guess(), this.evaluate(this.secret, this.guess())));
+            }
+        }
+    }
+
+    initializeGame = () => {
         this.tries(0);
+        this.counter(60);
         this.guess();
-        this.moves([]);       
-    }
-       
-    
-    winGame=(message)=>{
-        this.initializeGame();       
-        this.level(this.level()+1);
-        this.points(this.points()+1);
-
-        this.moves.push(new Move(this.secret,message));
-        let coef=0;
-        
-        coef=Number(this.level());
-        if (coef<10){
-            this.secret = this.createSecret(coef);
-            console.log(this.secret);
- 
-        }
-      
-
+        this.moves([]);
     }
 
-    looseGame=()=>{
-      
-        if (this.lives()>0){
-            this.initializeGame();                    
-            let message=`You could't find it and You have ${this.lives()} lives left.`;
-            this.moves.push(new Move(this.secret,message)); 
-                                                               
-            let coef=Number(this.level());
-            if (coef<10){
-                this.secret = this.createSecret(coef);
-                console.log(this.secret);
+    playerLevelsUp = (message) => {
+        this.initializeGame();
+        if (this.level() == 10) {
+            clearInterval(this.timer);
+            window.location.href = "playerwins.html";
+            return;
         }
-        
-        }        
-        else {                        
-            
-            this.stopGame();  
+        this.level(this.level() + 1);
+        this.moves.push(new Move(this.secret, `You win and continue with the next level ${this.level()}`));
+        this.secret = this.createSecret(this.level());
+    }
+
+    playerFails = () => {
+        this.lives(this.lives() - 1);
+        if (this.lives() > 0) {
+            this.initializeGame();
+            this.moves.push(new Move(this.secret, `You failed at the level ${this.level()}!`));
+            this.secret = this.createSecret(this.level());
+        } else {
+            this.gameIsOver();
         }
     }
 
-    createSecret = (coef) => {
-        let max=1;  
-        console.log(coef);      
-        while (coef>0){
-            max*=10;
-            coef-=1;
-        }  
-        let min=max/10;
-        max-=1;                
-        return Math.floor(Math.random() * (max - min + 1) + min)  
+    gameIsOver = () => {
+        clearInterval(this.timer);
+        window.location.href = "gameover.html";
     }
 
-    stopGame =()=>{
-        this.moves.push(new Move(this.secret,"You used your 3 lives"));
-        // if(this.level()===10){
+    createSecret = (level) => {
+        const digits = [];
+        digits.push(this.createRandomDigit(1, 9))
+        while (digits.length < level) {
+            const digit = this.createRandomDigit(0, 9);
+            if (digits.includes(digit)) continue;
+            digits.push(digit);
+        }
+        const secret = digits.reduce((number, digit) => number * 10 + digit, 0);
+        console.log(secret);
+        return secret;
+    }
 
-        // }
- 
-        
-
+    createRandomDigit = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
 }
-
 
 
 const mastermindViewModel = new MastermindViewModel();
